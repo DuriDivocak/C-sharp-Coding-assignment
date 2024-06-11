d27d718b40e47a1b2f3afb0771c058e7236fe1c3
Durinoo <juraj.rusnak00@gmail.com>
2024-06-11 14:11:20 +0200

    Quick fix

--- Csharp-coding-assignment/Program.cs
+++ Csharp-coding-assignment/Program.cs
@@ -66,7 +66,7 @@ namespace TimeReflector
         {
             if (input == "")
             {
-                throw new ArgumentException("input cannot be an empty string");
+                return "Invalid input!";
             }
             string finalOutput = "";
             string[] times = input.Split(";;");
